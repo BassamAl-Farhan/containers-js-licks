90fa86723930bfe07f536c64d5382a3c08231d4d
BassamAl-Farhan <bassamalfarhan@yahoo.com>
2022-09-18 20:42:47 -0700

    containers&js&licks

---
+++ script.js
@@ -0,0 +1,33 @@
+
+var count = 9 ;
+var nilemLikeElement = document.querySelector("#nilemLike")
+
+
+
+function add1() {
+    count++;
+    nilemLikeElement.innerText =  count + " like(s) ";
+    console.log(count);
+}
+
+var count1 = 12 ;
+var nicholekLikeElement = document.querySelector("#nicholekLike")
+
+
+
+function add2() {
+    count1++;
+    nicholekLikeElement.innerText =  count1 + " like(s) ";
+
+}
+
+var count2 = 9 ;
+var jimrLikeElement = document.querySelector("#jimrLike")
+
+
+
+function add3() {
+    count2++;
+    jimrLikeElement.innerText =  count2 + " like(s) ";
+
+}
